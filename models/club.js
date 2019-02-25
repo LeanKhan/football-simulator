2994f8142b00d0892df1870be38a5c4983e1e0ab
LeanKhan <eslean20@gmail.com>
2019-02-25 12:27:11 +0100

    Create Club model

---
+++ models/club.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ClubSchema = new Schema({
+  Name: {
+    type: String,
+    required: true
+  },
+  ClubCode: {
+    type: String,
+    required: true
+  },
+  AttackingClass: {
+    type: Number,
+    required: true
+  },
+  DefensiveClass: {
+    type: Number,
+    required: true
+  },
+  Manager: String,
+  Stadium: String,
+  League: String
+});
+
+module.exports = Club = mongoose.model("Club", ClubSchema, "Clubs");
