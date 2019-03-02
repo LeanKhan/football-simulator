9a20ca4e316bb5d4e683a2f6880feeb70e1546bb
LeanKhan <eslean20@gmail.com>
2019-03-02 19:19:38 +0100

    Display league table for selected league

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -27,6 +27,46 @@ var model = {
   },
   makeFixtures() {
     if (season.LeagueCode == "L1") {
+      // Week 1
+      fixtures.push(new Match(clubs[0],clubs[1]).details);
+      fixtures.push(new Match(clubs[2],clubs[3]).details);
+      fixtures.push(new Match(clubs[4],clubs[5]).details);
+      // Week 2
+      fixtures.push(new Match(clubs[5],clubs[1]).details);
+      fixtures.push(new Match(clubs[0],clubs[3]).details);
+      fixtures.push(new Match(clubs[2],clubs[4]).details);
+      // Week 3
+      fixtures.push(new Match(clubs[1],clubs[3]).details);
+      fixtures.push(new Match(clubs[4],clubs[0]).details);
+      fixtures.push(new Match(clubs[5],clubs[2]).details);
+      // Week 4
+      fixtures.push(new Match(clubs[3],clubs[4]).details);
+      fixtures.push(new Match(clubs[1],clubs[2]).details);
+      fixtures.push(new Match(clubs[0],clubs[5]).details);
+      // Week 5
+      fixtures.push(new Match(clubs[2],clubs[0]).details);
+      fixtures.push(new Match(clubs[4],clubs[1]).details);
+      fixtures.push(new Match(clubs[3],clubs[5]).details);
+      // Week 6
+      fixtures.push(new Match(clubs[1],clubs[0]).details);
+      fixtures.push(new Match(clubs[3],clubs[2]).details);
+      fixtures.push(new Match(clubs[5],clubs[4]).details);
+      // Week 7
+      fixtures.push(new Match(clubs[1],clubs[5]).details);
+      fixtures.push(new Match(clubs[3],clubs[0]).details);
+      fixtures.push(new Match(clubs[4],clubs[2]).details);
+      // Week 8
+      fixtures.push(new Match(clubs[3],clubs[1]).details);
+      fixtures.push(new Match(clubs[0],clubs[4]).details);
+      fixtures.push(new Match(clubs[2],clubs[5]).details);
+      // Week 9
+      fixtures.push(new Match(clubs[4],clubs[3]).details);
+      fixtures.push(new Match(clubs[2],clubs[1]).details);
+      fixtures.push(new Match(clubs[5],clubs[0]).details);
+      // Week 10
+      fixtures.push(new Match(clubs[0],clubs[2]).details);
+      fixtures.push(new Match(clubs[1],clubs[4]).details);
+      fixtures.push(new Match(clubs[5],clubs[3]).details);
     } else {
       // League 3 and 2 Fixtures
       // Weeks 1 - 3
--- scripts/league_table.js
+++ scripts/league_table.js
@@ -2,6 +2,7 @@
 var selected_league = "";
 var selected_league_code = "";
 var seasons;
+var standings = [];
 
 var model = {
   getLatestSeason(league_code) {
@@ -10,11 +11,89 @@ var model = {
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
         seasons = JSON.parse(xhttp.response);
+        // standings = seasons[0].Standings;
+        model.sortStandings(seasons[0].Standings);
+
+        // view.displayStandings(seasons[0].Standings);
       }
     };
     xhttp.open("GET", `/data/league/${league_code}/seasons`, true);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send();
+  },
+  sortStandings(standings_array) {
+    standings = standings_array.sort((a, b) => {
+      if (b.Points == a.Points) {
+        if (b.GD == b.GD) {
+          return b.GF - a.GF;
+        } else {
+          b.GD - a.GD;
+        }
+      } else {
+        return b.Points - a.Points;
+      }
+    });
+    console.log(standings);
+    view.displayStandings(standings);
+  }
+};
+
+var view = {
+  displayStandings(standings) {
+    let tbody = document.getElementById("tbody");
+    view.clearTable();
+    standings.forEach((team, i) => {
+      // Create Elements
+      let tr = document.createElement("tr");
+      let pos = document.createElement("td");
+      let club_element = document.createElement("td");
+      let played = document.createElement("td");
+      let w = document.createElement("td");
+      let l = document.createElement("td");
+      let d = document.createElement("td");
+      let gf = document.createElement("td");
+      let ga = document.createElement("td");
+      let gd = document.createElement("td");
+      let points_element = document.createElement("td");
+
+      pos.innerHTML = `<b>${i + 1}</b>`;
+      club_element.innerHTML = `<img src='/img/${
+        team.TeamCode
+      }.png' height='45px'><span>${team.Team}</span>`;
+      played.innerHTML = `${team.Played}`;
+      w.innerHTML = `${team.Wins}`;
+      l.innerHTML = `${team.Losses}`;
+      d.innerHTML = `${team.Draws}`;
+      gf.innerHTML = `${team.GF}`;
+      ga.innerHTML = `${team.GA}`;
+      gd.innerHTML = `${team.GD}`;
+      points_element.innerHTML = `${team.Points}`;
+
+      // Conditions
+
+      if (i == 0) {
+        tr.setAttribute("class", "bg-success");
+      } else if (i == standings.length - 1) {
+        tr.setAttribute("class", "bg-warning");
+      }
+
+      tr.appendChild(pos);
+      tr.appendChild(club_element);
+      tr.appendChild(played);
+      tr.appendChild(w);
+      tr.appendChild(l);
+      tr.appendChild(d);
+      tr.appendChild(gf);
+      tr.appendChild(ga);
+      tr.appendChild(gd);
+      tr.appendChild(points_element);
+
+      tbody.appendChild(tr);
+    });
+  },
+  clearTable() {
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
   }
 };
 
